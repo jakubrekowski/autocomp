3c8c6c264d355ff041ee1ffd14a3c513eede0560
Jakub Rekowski <jakub.rekowski@gmail.com>
2020-01-25 23:14:30 +0000

    Update to v0.13

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "autocomp v0.13 | https://gitlab.com/jakub.rekowski/autocomp\n\n";
+    system("g++ *.cpp -o main && ./main");
+    return 0;
+}
