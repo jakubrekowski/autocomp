a8f6aad3be1899874bb548f0726bb3526d8075f5
Jakub Rekowski <jakub.rekowski@gmail.com>
2020-01-30 07:06:13 +0100

    Update to add args (actualy not working)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
-int main() {
-    cout << "autocomp v0.13 | https://gitlab.com/jakub.rekowski/autocomp\n\n";
-    system("g++ *.cpp -o main && ./main");
+int main(int argc, char **argv) {
+    cout << "autocomp v0.13 | https://github.com/rekubrekowski/autocomp\n\n";
+  
+    if (argv[1] == 0)
+    {
+        cout << "You haven't define any files";
+        system("g++ *.cpp -o main && ./main");
+    }
+    else
+    {
+        system(("g++ " + argv[1] + " -o main && ./main").c_str());
+    }    
+  
     return 0;
 }
